2fe4b6605fad81da9fd157d9a698033c7bfc76ff
OS 046209 <yonarobal>
2022-05-18 18:59:34 +0300

    latest bank

--- hw2/bank.cpp
+++ hw2/bank.cpp
@@ -13,17 +13,81 @@ main file. This file contains the main function of smash
 #include <iostream>
 
 using namespace std;
-extern vector<accounts> accounts
+
+bank::bank(int bankMoney): bankMoney(0), bandReaders(0), listReaders(0){
+	 pthread_mutex_init(&globalBankLock ,nullptr);
+	 pthread_mutex_init(&bankReadLock ,nullptr);
+	 pthread_mutex_init(&bankWriteLock ,nullptr);
+	 pthread_mutex_init(&listReadLock ,nullptr);
+	 pthread_mutex_init(&listWriteLock ,nullptr);
+}
+
+bank::~bank(){
+	 pthread_mutex_destreoy(&globalBankLock);
+	 pthread_mutex_destreoy(&bankReadLock);
+	 pthread_mutex_destreoy(&bankWriteLock);
+	 pthread_mutex_destreoy(&listReadLock);
+	 pthread_mutex_destreoy(&listWriteLock);
+}
+
+void bank::LockListRead() {
+    pthread_mutex_lock(&listReadLock);
+    listReaders++;
+    if(listReaders == 1){
+        pthread_mutex_lock(&listWriteLock);
+    }
+    pthread_mutex_unlock(&listReadLock);
+}
+
+void bank::UnlockListRead() {
+    pthread_mutex_lock(&listReadLock);
+    listReaders--;
+    if(!listReaders){
+        pthread_mutex_unlock(&listWriteLock);
+    }
+    pthread_mutex_unlock(&listReadLock);
+}
 
 
-bank::bank(int bankMoney): bankMoney(0){
+void bank::LockListWrite() {
+    pthread_mutex_lock(&listWriteLock);
+}
 
+void bank::UnlockListWrite() {
+    pthread_mutex_unlock(&listWriteLock);
+}
+
+void bank::LockBankRead() {
+    pthread_mutex_lock(&bankReadLock);
+    bankReaders++;
+    if(bankReaders == 1){
+        pthread_mutex_lock(&bankWriteLock);
+    }
+    pthread_mutex_unlock(&bankReadLock);
+}
+
+void bank::UnlockListRead() {
+    pthread_mutex_lock(&bankReadLock);
+    bankReaders++;
+    if(!bankReaders){
+        pthread_mutex_unlock(&bankWriteLock);
+    }
+    pthread_mutex_unlock(&bankReadLock);
+}
+
+void bank::LockBankWrite(){
+	pthread_mutex_lock(&bankWriteLock);
+}
+
+void bank::UnlockBankWrite(){
+	pthread_mutex_unlock(&bankWriteLock);
 }
 
 bank::getCommision(){
 	double commisionPrecent = 3/100;
+	LockListRead();
 	for(unsigned int i=0; i<accounts.size(); i++){
-		double CommisionAmmount =  accounts[i].commisionToBank(accounts[i].accountId);
+		accounts[i].ReadLock.(accounts[i].accountId);
 		bankMoney += CommisionAmmount;
 		}
 	}
@@ -31,7 +95,7 @@ bank::getCommision(){
 }
 
 bank::statusPrint(){
-
+	
 
 
 
--- hw2/bank.h
+++ hw2/bank.h
@@ -9,16 +9,30 @@
 #include <sys/wait.h>
 #include <pthread.h>
 #include <vector>
-
-using namespace std;
+using namespace std
 
 class bank {
 	private:
-	double BankMoney;
+	//pthread_mutex_t globalBankLock;
+	int bankReaders;
+	int listReaders;
+    	pthread_mutex_t bankReadLock;
+    	pthread_mutex_t bankWriteLock;
+    	pthread_mutex_t listReadLock;
+    	pthread_mutex_t listWriteLock;
 
 	public:
 	void getCommissions(int _accountId, int _password, double _balance);
-	void statusPrint(int _accountId, int _password, double _balance);	
+	void statusPrint(int _accountId, int _password, double _balance);
+	void LockListRead();
+	void UnockListRead();
+	void LockListwrite();
+	void UnockListwrite();
+	void LockBankRead();
+	void UnlockBankRead();
+	void LockBankWrite();	
+	void UnlockBankWrite();
+	double BankMoney;
 };
 
 #endif
