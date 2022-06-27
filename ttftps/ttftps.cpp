8c8a0c5b482535418cccf2afe2892e67cad5b6a9
On Fabian <onfabian@campus.technion.ac.il>
2022-06-27 21:31:33 +0300

    FINISHgit status!

--- ttftps/ttftps.cpp
+++ ttftps/ttftps.cpp
@@ -1,16 +1,23 @@
+#include "ttftps.h"
 #include <iostream>
 #include <cstring>
 #include <vector>
 #include <list>
 
-#include "ttftps.h"
-
 using namespace std;
+
+bool Client::operator!=(const Client &s) const {
+    return !( (client_address.sin_addr.s_addr == s.client_address.sin_addr.s_addr) && (client_address.sin_port == s.client_address.sin_port) );
+}
+
+bool Client::operator==(const Client &s) const {
+    return ( (client_address.sin_addr.s_addr == s.client_address.sin_addr.s_addr) && (client_address.sin_port == s.client_address.sin_port) );
+}
 
 Client::Client(int socket, int num) {
     client_address_len = (socklen_t)sizeof(client_address);
     this->socket = socket;
-    timeout_expired_counter = 0;
+    timeout_counter = 0;
     block_counter = 0;
 
     this->num = num;
@@ -18,137 +25,6 @@ Client::Client(int socket, int num) {
     mode = nullptr;
     mode_size = 0;
     file_d = 0;
-
-}
-
-bool Client::operator==(const Client &s) const {
-    return ( (client_address.sin_addr.s_addr == s.client_address.sin_addr.s_addr) && (client_address.sin_port == s.client_address.sin_port) );
-}
-
-bool Client::operator!=(const Client &s) const {
-    return !( (client_address.sin_addr.s_addr == s.client_address.sin_addr.s_addr) && (client_address.sin_port == s.client_address.sin_port) );
-}
-
-
-void send_error_message(Client *client, int error_code){
-    struct ERROR error_packet = {0};
-    error_packet.opcode = htons(ERROR_OPCODE);
-
-    switch (error_code) {
-        case UNKNOWN_USER_ERR_CODE: { //OPCODE = 7 
-            error_packet.ErrorCode = htons(UNKNOWN_USER_ERR_CODE);
-            strcpy(error_packet.strings, "Unknown user");
-            int size = 4 + strlen(error_packet.strings) + 1;
-            if (sendto(client->socket, &error_packet, size, 0,
-                       (struct sockaddr *)&client->client_address, client->client_address_len) != size) {
-                perror("TTFTP_ERROR:");
-                exit(1);
-            }
-
-            cout << "Unknown user" << endl;
-            break;
-        }
-
-        case UNEXPECTED_PCKT_ERR_CODE: {
-            error_packet.ErrorCode = htons(UNEXPECTED_PCKT_ERR_CODE);
-            strcpy(error_packet.strings, "Unexpected packet");
-            int size = 4 + strlen(error_packet.strings) + 1;
-            if (sendto(client->socket, &error_packet, size, 0,
-                       (struct sockaddr *)&client->client_address, client->client_address_len) != size) {
-                perror("TTFTP_ERROR:");
-                exit(1);
-            }
-
-            cout << "Unexpected packet" << endl;
-            break;
-        }
-
-        case FILE_ALREADY_EXIST_ERR_CODE: { // OPCODE = 6 
-            error_packet.ErrorCode = htons(FILE_ALREADY_EXIST_ERR_CODE);
-            strcpy(error_packet.strings, "File already exists");
-            int size = 4 + strlen(error_packet.strings) + 1;
-            if (sendto(client->socket, &error_packet, size, 0,
-                       (struct sockaddr *)&client->client_address, client->client_address_len) != size) {
-                perror("TTFTP_ERROR:");
-                exit(1);
-            }
-
-            cout << "File already exists" << endl;
-            break;
-        }
-
-        case BAD_BLOCK_NUM_ERR_CODE: { // OPCODE = 0
-            error_packet.ErrorCode = htons(0);
-            strcpy(error_packet.strings, "Bad block number");
-            int size = 4 + strlen(error_packet.strings) + 1;
-            if (sendto(client->socket, &error_packet, size, 0,
-                       (struct sockaddr *)&client->client_address, client->client_address_len) != size) {
-                perror("TTFTP_ERROR:");
-                exit(1);
-            }
-
-            cout << "Bad block number" << endl;
-            break;
-        }
-
-        case ABANDONING_TRANS: { // OPCODE = 0
-            error_packet.ErrorCode = htons(0);
-            strcpy(error_packet.strings, "Abandoning file transmission");
-            int size = 4 + strlen(error_packet.strings) + 1;
-            if (sendto(client->socket, &error_packet, size, 0,
-                       (struct sockaddr *)&client->client_address, client->client_address_len) != size) {
-                perror("TTFTP_ERROR:");
-                exit(1);
-            }
-
-            cout << "Abandoning file transmission" << endl;
-            break;
-        }
-
-        default: ;
-            //not supposed to reach here
-            cout << "Unknown error code" << endl;
-    }
-}
-
-
-bool isValidNumber(char number[])
-{
-    //checking if a negative number
-    if (number[0] == '-') return false;
-
-    for (int i = 0; number[i] != 0; i++)
-    {
-        if (!isdigit(number[i]))
-            return false;
-    }
-    return true;
-}
-
-void check_valid_input(int argc, char *argv[]){
-    // Checking if valid number of arguments
-    if (argc != 4) {
-        perror("TTFT_ERROR: arguments illegal");
-        exit(1);
-    }
-
-    // Checking valid port number
-    if (!isValidNumber(argv[1])) {
-        perror("TTFT_ERROR: illegal arguments");
-        exit(1);
-    }
-
-    // Checking valid timeout number
-    if (!isValidNumber(argv[2])) {
-        perror("TTFT_ERROR: illegal arguments");
-        exit(1);
-    }
-
-    // Checking valid port number
-    if (!isValidNumber(argv[3])) {
-        perror("TTFT_ERROR: illegal arguments");
-        exit(1);
-    }
 }
 
 void parsing_wrq(Client *client, struct WRQ *wrq){
@@ -164,8 +40,122 @@ void parsing_wrq(Client *client, struct WRQ *wrq){
     client->mode[client->mode_size] = '\0';
     strcpy(client->mode, wrq->strings + 1 + strlen(client->file_name));
 
-    cout << "WRQ parsing succeed" << endl;
+}
 
+void send_error_message(Client *client, int error_code){
+    struct ERROR error_packet = {0};
+    error_packet.opcode = htons(ERROR_OPCODE);
+
+    switch (error_code) {
+
+        case UNKNOWN_USER_ERR_CODE: { //OPCODE = 7 
+            error_packet.ErrorCode = htons(UNKNOWN_USER_ERR_CODE);
+            strcpy(error_packet.strings, "Unknown user");
+            int size = 4 + strlen(error_packet.strings) + 1;
+            if (sendto(client->socket, &error_packet, size, 0,
+                       (struct sockaddr *)&client->client_address, client->client_address_len) != size) {
+                perror("TTFTP_ERROR:");
+                exit(1);
+            }
+
+            break;
+        }
+
+        case UNEXPECTED_PCKT_ERR_CODE: {
+            error_packet.ErrorCode = htons(UNEXPECTED_PCKT_ERR_CODE);
+            strcpy(error_packet.strings, "Unexpected packet");
+            int size = 4 + strlen(error_packet.strings) + 1;
+            if (sendto(client->socket, &error_packet, size, 0,
+                       (struct sockaddr *)&client->client_address, client->client_address_len) != size) {
+                perror("TTFTP_ERROR:");
+                exit(1);
+            }
+
+            break;
+        }
+
+        case FILE_ALREADY_EXIST_ERR_CODE: { // OPCODE = 6 
+            error_packet.ErrorCode = htons(FILE_ALREADY_EXIST_ERR_CODE);
+            strcpy(error_packet.strings, "File already exists");
+            int size = 4 + strlen(error_packet.strings) + 1;
+            if (sendto(client->socket, &error_packet, size, 0,
+                       (struct sockaddr *)&client->client_address, client->client_address_len) != size) {
+                perror("TTFTP_ERROR:");
+                exit(1);
+            }
+
+            break;
+        }
+
+        case BAD_BLOCK_NUM_ERR_CODE: { // OPCODE = 0
+            error_packet.ErrorCode = htons(0);
+            strcpy(error_packet.strings, "Bad block number");
+            int size = 4 + strlen(error_packet.strings) + 1;
+            if (sendto(client->socket, &error_packet, size, 0,
+                       (struct sockaddr *)&client->client_address, client->client_address_len) != size) {
+                perror("TTFTP_ERROR:");
+                exit(1);
+            }
+
+            break;
+        }
+
+        case ABANDONING_TRANS: { // OPCODE = 0
+            error_packet.ErrorCode = htons(0);
+            strcpy(error_packet.strings, "Abandoning file transmission");
+            int size = 4 + strlen(error_packet.strings) + 1;
+            if (sendto(client->socket, &error_packet, size, 0,
+                       (struct sockaddr *)&client->client_address, client->client_address_len) != size) {
+                perror("TTFTP_ERROR:");
+                exit(1);
+            }
+
+            break;
+        }
+
+        default: ;
+            //not supposed to reach here
+    }
+}
+
+
+bool isValid(char number[]) {
+    //checking if a negative number
+    if (number[0] == '-') return false;
+    int val = atoi(number);
+    if (!val) return false;
+    for (int i = 0; number[i] != 0; i++) {
+        if (!isdigit(number[i])) {
+            return false;
+	}
+    }
+    return true;
+}
+
+void check_valid(int argc, char *argv[]){
+    // Checking if valid number of arguments
+    if (argc != 4) {
+        cout << "TTFT_ERROR: illegal arguments" << endl;
+        exit(1);
+    }
+
+    // Checking valid port number
+    if (!isValid(argv[1])) {
+        cout << "TTFT_ERROR: illegal arguments" << endl;
+        exit(1);
+    }
+
+    // Checking valid timeout number
+    if (!isValid(argv[2])) {
+        cout << "TTFT_ERROR: illegal arguments" << endl;
+        exit(1);
+    }
+
+    // Checking valid error number
+    if (!isValid(argv[3])) {
+        cout << "TTFT_ERROR: illegal arguments" << endl;
+        exit(1);
+    }
 }
 
 void send_ack(Client *client){
@@ -180,7 +170,6 @@ void send_ack(Client *client){
         exit(1);
     }
 
-    cout << "ACK " << client->block_counter << " was sent" << endl;
 
     client->block_counter++;
 }
@@ -200,14 +189,13 @@ void close_file(char* file_name, int fd) {
 
 
 int main(int argc,char *argv[]) {
-    check_valid_input(argc, argv);
+    check_valid(argc, argv);
 
     int server_socket;
     struct sockaddr_in server_address;
     struct timeval timeout_const;
     struct timeval timeout;
 
-    // receiving input - Port number, timeout time and number of possible errors
     unsigned short ServPort = atoi(argv[1]);
     timeout_const.tv_sec = atoi(argv[2]);
     timeout_const.tv_usec = 0;
@@ -240,7 +228,7 @@ int main(int argc,char *argv[]) {
     while (1) {
 
 	bool not_empty = false;
-	cout << number_of_clients << endl;
+	//cout << number_of_clients << endl;
         fd_set read_fds;
         FD_ZERO(&read_fds);
         FD_CLR(server_socket, &read_fds);
@@ -260,28 +248,24 @@ int main(int argc,char *argv[]) {
             timersub(&now, &next_timeout_expired_client->last_ack_time, &timeout);
             timersub(&timeout_const,&timeout,&timeout);
 
-            cout << "next timeout is in: " << timeout.tv_sec << " sec and " << timeout.tv_usec << "usec" << endl;
-
             select_return = select(server_socket + 1, &read_fds, NULL, NULL, &timeout);
         }
 
         if (select_return > 0) {
-            cout << "select() > 0, waiting message detected" << endl;
 
             received_message_size = recvfrom(server_socket, &general_packet, MAX_PACKET_SIZE, 0,
                                              (struct sockaddr *) &curr_client->client_address, &curr_client->client_address_len);
 
             if (received_message_size < 0) {
                 perror("TTFTP_ERROR:");
-                //need to check how handle
                 exit(1);
             }
-	    cout << "client_list.size() " << client_list.size() << endl;
+	    //cout << "client_list.size() " << client_list.size() << endl;
 	    if (client_list.size() > 0) {
 		    not_empty = true;
 	    }
 
-            //check if client is exist
+            //check if client is exist for ERROR 4
             auto it = client_list.begin();
             client_exist = false;
             for (; it != client_list.end(); it++) {
@@ -292,11 +276,10 @@ int main(int argc,char *argv[]) {
                 }
             }
 
-            if (!client_exist) { // if client does not exist
-                cout << "client was not recognized" << endl;
+            if (!client_exist) { // client does not exist
 
                 unsigned short int opcode = ntohs(general_packet.opcode);
-		cout << "CURR_CLIENT_NUM:!!!!!!! " << curr_client->num << endl;
+		//cout << "CURR_CLIENT_NUM:!!!!!!! " << curr_client->num << endl;
 	   	if (not_empty) {
                    	send_error_message(curr_client, UNEXPECTED_PCKT_ERR_CODE);
                    	continue;
@@ -307,8 +290,6 @@ int main(int argc,char *argv[]) {
                     continue;
                 }
 
-                // opcode == WRQ_OPCODE
-                cout << "new WRQ detected" << endl;
 
                 struct WRQ wrq = {0};
                 memcpy(&wrq, &general_packet, sizeof(general_packet));
@@ -319,7 +300,6 @@ int main(int argc,char *argv[]) {
                 // checking if file exists
                if(access(file_path,F_OK) == 0){
                     send_error_message(curr_client, FILE_ALREADY_EXIST_ERR_CODE);
-                    cout << "File already exist" << endl;
                     continue;
                 }
 
@@ -331,7 +311,6 @@ int main(int argc,char *argv[]) {
                     exit(1);
                 }
 
-                cout << "connection established" << endl;
                 // send ack back
                 send_ack(curr_client);
                 gettimeofday(&curr_client->last_ack_time, NULL);
@@ -342,7 +321,7 @@ int main(int argc,char *argv[]) {
             } // end of client does not exist block
 
             else { // client does exist
-                cout << "Client has open connection, expecting DATA packet" << endl;
+
                 curr_client = &(*it); // attach the existing client to curr_client variable
 
                 unsigned short int opcode = ntohs(general_packet.opcode);
@@ -356,15 +335,13 @@ int main(int argc,char *argv[]) {
                     continue;
                 }
 
-                cout << "Packet was recognized as DATA packet" << endl;
-
                 // DATA packet
                 struct DATA data_packet = {0};
                 memcpy(&data_packet, &general_packet, sizeof(general_packet));
 
                 // Bad block number
                 if (ntohs(data_packet.block_number) != curr_client->block_counter) {
-                    cout << "Bad blockk number!" << endl;
+
                     send_error_message(curr_client, BAD_BLOCK_NUM_ERR_CODE);
                     close_file(curr_client->file_name, curr_client->file_d);
                     client_list.erase(it);
@@ -384,8 +361,6 @@ int main(int argc,char *argv[]) {
                 int data_size = received_message_size - 4;
                 int write_size = write(curr_client->file_d, data_packet.data, data_size);
 
-                cout << write_size << " was written from last packet" << endl;
-
                 if (write_size != data_size) {
                     perror("TTFTP_ERROR:"); // NEED TO CHECK
                     exit(1);
@@ -399,12 +374,12 @@ int main(int argc,char *argv[]) {
 
                 // Last packet from client
                 if (received_message_size < 516) {
-                    cout << "Packet size is less than 516 Bytes, so this is the last packet" << endl;
+
                     if (close(curr_client->file_d) == -1) {
                         perror("TTFTP_ERROR:");
                         exit(1);
                     }
-                    cout << "Client was removed" << endl;
+
                     client_list.pop_back();
                     number_of_clients--;
                 }
@@ -412,15 +387,15 @@ int main(int argc,char *argv[]) {
             }
         } // end of (select_return > 0) block
 
-        else if (select_return == 0) { // timeout expired
-            cout << "TIMEOUT" << endl;
+        else if (select_return == 0) { 
+
             auto it = client_list.begin();
             Client *next_time_expired_client = &(*it);
 
-            next_time_expired_client->timeout_expired_counter++;
+            next_time_expired_client->timeout_counter++;
 
-            if (next_time_expired_client->timeout_expired_counter > number_of_error_const) {
-                cout << "MAX TIMEOUT REACHED, client is boiled out" << endl;
+            if (next_time_expired_client->timeout_counter > number_of_error_const) {
+
                 send_error_message(next_time_expired_client, ABANDONING_TRANS);
                 close_file(next_time_expired_client->file_name,next_time_expired_client->file_d);
                 client_list.pop_front();
@@ -428,23 +403,22 @@ int main(int argc,char *argv[]) {
                 continue;
 
             } else {
-                cout << "sending last ack again" << endl;
-                next_time_expired_client->block_counter--; // send_ack() send the next ack
+
+                next_time_expired_client->block_counter--; 
                 send_ack(next_time_expired_client);
                 gettimeofday(&next_time_expired_client->last_ack_time, NULL);
-                // move element to the end of the list
+
                 client_list.splice( client_list.end(), client_list, it);
             }
         }
 
         if (select_return < 0) {
-            // sys call error, client boiled out
             perror("TTFTP_ERROR:");
             exit(1);
         }
     } //end of while(1)
 
-    // close all files
+    // close file
     for (auto it = client_list.begin(); it != client_list.end(); it++) {
         close_file(it->file_name,it->file_d);
     }
--- ttftps/ttftps.h
+++ ttftps/ttftps.h
@@ -1,7 +1,3 @@
-//
-// Created by os on 17.1.2022.
-//
-
 #include <cstdio>
 #include <iostream>
 #include <stdio.h>
@@ -18,29 +14,22 @@
 #include <sys/time.h>
 #include <fstream>
 
+#define ABANDONING_TRANS -1
+#define BAD_BLOCK_NUM_ERR_CODE 0
 #define WRQ_OPCODE 2
 #define DATA_OPCODE 3
 #define ACK_OPCODE 4
+#define UNEXPECTED_PCKT_ERR_CODE 4
 #define ERROR_OPCODE 5
-#define BUFFER_MAX_SIZE 1500
+#define FILE_ALREADY_EXIST_ERR_CODE 6
+#define UNKNOWN_USER_ERR_CODE 7
 #define OCTET "octet"
 #define MAX_PACKET_SIZE 1500
 #define MAX_DATA_SIZE 512
 #define MAX_DATA_PACKET_SIZE 516
-#define UNKNOWN_USER_ERR_CODE 7
-#define UNEXPECTED_PCKT_ERR_CODE 4
-#define FILE_ALREADY_EXIST_ERR_CODE 6
-#define BAD_BLOCK_NUM_ERR_CODE 0
-#define ABANDONING_TRANS -1
 
 
 using namespace std;
-
-struct ACK {
-    unsigned short int opcode;
-    unsigned short int block_number;
-} __attribute__((packed));
-
 
 struct WRQ {
     unsigned short int opcode;
@@ -55,17 +44,21 @@ struct DATA {
 
 } __attribute__((packed));
 
-struct ERROR {
-    unsigned short int opcode;
-    unsigned short int ErrorCode;
-    char strings[MAX_PACKET_SIZE];
-} __attribute__((packed));
-
 struct PACKET {
     unsigned short int opcode;
     char strings[MAX_PACKET_SIZE];
 } __attribute__((packed));
 
+struct ACK {
+    unsigned short int opcode;
+    unsigned short int block_number;
+} __attribute__((packed));
+
+struct ERROR {
+    unsigned short int opcode;
+    unsigned short int ErrorCode;
+    char strings[MAX_PACKET_SIZE];
+} __attribute__((packed));
 
 
 class Client {
@@ -86,10 +79,10 @@ public:
 
     struct timeval last_ack_time;
 
-    int timeout_expired_counter;
-
-    bool operator == (const Client& s) const;
+    int timeout_counter;
     bool operator != (const Client& s) const;
+    bool operator == (const Client& s) const;
+
 
 
 };
